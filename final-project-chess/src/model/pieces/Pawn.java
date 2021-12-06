a10cc0f296292bea9dbd5383666f7e21b459d133
harshvardhanb25 <harshvardhanb25@email.arizona.edu>
2021-12-06 11:41:06 -0700

    Created the scene. Need to add squares and pieces.

--- final-project-chess/src/model/pieces/Pawn.java
+++ final-project-chess/src/model/pieces/Pawn.java
@@ -2,6 +2,7 @@ package model.pieces;
 
 public class Pawn extends ChessPiece {
 
+	//TODO: For pawns we need to make a note if the piece moved 2 paces in the first move to check if an en-passant can happen. I think that a boolean would be best.
 	public Pawn(int row, int col, int color) {
 		super(row, col, color);
 	}
