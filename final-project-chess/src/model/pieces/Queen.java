ff41000880ce6cbe11096f3062eb30eb1571b62c
jasnamswani <jswani@email.arizona.edu>
2021-12-06 19:18:19 -0700

    updated queen

--- final-project-chess/src/model/pieces/Queen.java
+++ final-project-chess/src/model/pieces/Queen.java
@@ -27,7 +27,6 @@ public class Queen extends ChessPiece {
 					return false;
 				}
 			}
-			setPosition(row, col);
 			return true;
 		}
 		
@@ -45,7 +44,6 @@ public class Queen extends ChessPiece {
 					return false;
 				}
 			}
-			setPosition(row, col);
 			return true;
 		}
 		
@@ -79,7 +77,6 @@ public class Queen extends ChessPiece {
 			curCol += colOff;
 			curRow += rowOff;
 		}
-		setPosition(row, col);
 		return true;
 	}
 
