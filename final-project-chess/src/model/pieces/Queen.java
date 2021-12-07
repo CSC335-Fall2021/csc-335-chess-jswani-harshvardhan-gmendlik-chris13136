13dd6621da443189504574f94ee099a140099f73
gmendlik <garrisonmendlik@yahoo.com>
2021-12-07 12:52:00 -0700

    Fixed queen error

--- final-project-chess/src/model/pieces/Queen.java
+++ final-project-chess/src/model/pieces/Queen.java
@@ -9,19 +9,19 @@ public class Queen extends ChessPiece {
 	@Override
 	public boolean isValidMove(int row, int col, ChessPiece[][] pieces) {
 		// TODO Auto-generated method stub
-		
-		if(pieces[col][row].getColor() == this.color) {
+
+		if (pieces[col][row].getColor() == this.color) {
 			return false;
 		}
-		
-		if(this.row == row) {
-			//right check
+
+		if (this.row == row) {
+			// right check
 			for (int i = this.col; i < col; i++) {
 				if (pieces[i][row] != null) {
 					return false;
 				}
 			}
-			//left check
+			// left check
 			for (int i = this.col; i > col; i--) {
 				if (pieces[i][row] != null) {
 					return false;
@@ -29,16 +29,16 @@ public class Queen extends ChessPiece {
 			}
 			return true;
 		}
-		
-		//up down check
-		if(this.col == col) {
-			//up check
+
+		// up down check
+		if (this.col == col) {
+			// up check
 			for (int i = this.row; i < row; i++) {
 				if (pieces[col][i] != null) {
 					return false;
 				}
 			}
-			//down check
+			// down check
 			for (int i = this.row; i > row; i--) {
 				if (pieces[col][i] != null) {
 					return false;
@@ -46,30 +46,30 @@ public class Queen extends ChessPiece {
 			}
 			return true;
 		}
-		
-		checkDiagnols(row,col,pieces);
-		
+
+		checkDiagnols(row, col, pieces);
+
 		return false;
 	}
-	
-	public boolean checkDiagnols(int row, int col, ChessPiece[][] piece) {
-		if(this.row == row || this.col == col) {
+
+	public boolean checkDiagnols(int row, int col, ChessPiece[][] pieces) {
+		if (this.row == row || this.col == col) {
 			return false;
 		}
 		// default to +,+
 		int colOff = 1;
 		int rowOff = 1;
-		
+
 		if (Integer.signum(this.row - row) == 1) {
 			rowOff = -1;
 		}
-		
+
 		if (Integer.signum(this.col - col) == 1) {
 			colOff = -1;
 		}
 		int curCol = this.col;
 		int curRow = this.row;
-		
+
 		while (curCol != col) {
 			if (pieces[curCol][curRow] != null) {
 				return false;
