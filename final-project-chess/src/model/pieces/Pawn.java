86436dc8d6a24075424d8dd1dd00c190fde759a6
chrisbrinkley <chrisbrinkley@192.168.0.143>
2021-12-08 01:10:06 -0700

    wrote tests for all the peices

--- final-project-chess/src/model/pieces/Pawn.java
+++ final-project-chess/src/model/pieces/Pawn.java
@@ -10,7 +10,6 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public boolean isValidMove(int row, int col, ChessPiece[][] pieces) {
-		System.out.println("pawn moving to " + pieces[col][row]);
 		if(pieces[col][row] != null && pieces[col][row].getColor() == this.color) {
 			return false;
 		}
@@ -19,15 +18,16 @@ public class Pawn extends ChessPiece {
 		if (this.color == 1) {
 			offset = -1;
 		}
-		System.out.println("before row+off");
 		if (this.row + offset == row) {
-			System.out.println("inside row+off");
 			//if the square is empty then we all good
-			if(pieces[col][row] == null) {
-				System.out.println("returned true");
+			if(pieces[col][row] == null && this.col == col) {
 				return true;
 			}
-			//else it has a piece so false
+			else if( pieces[col][row] != null && pieces[col][row].color != this.color) {
+				//diagonal take check
+				return true;
+			}
+			//else it has a friendly piece so false
 			return false;
 		}
 		
@@ -48,20 +48,6 @@ public class Pawn extends ChessPiece {
 			return false;
 		}
 
-		// diagonal +1 check
-		if (col + 1 < 8 && this.row + offset == row && 
-				this.col + 1 == col && !(pieces[col][row] == null)) {
-			
-			return true;
-			
-		}
-		// diagonal -1 check
-		if (col - 1 > 0 && this.row + offset == row && 
-				this.col - 1 == col && !(pieces[col][row] == null)) {
-			
-			return true;
-			
-		}
 		// all 4 things a pawn can do failed so everything else is false
 		return false;
 	}
