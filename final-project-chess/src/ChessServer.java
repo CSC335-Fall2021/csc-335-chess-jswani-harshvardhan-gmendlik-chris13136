69e6722413dbc55074fc316c90d6ca18fb53a641
jasnamswani <jswani@email.arizona.edu>
2021-12-08 10:49:31 -0700

    adding server class

---
+++ final-project-chess/src/ChessServer.java
@@ -0,0 +1,10 @@
+public class ChessServer extends Thread
+{
+    public static void main(String[] args) throws Exception 
+    {
+    	ChessServer server = new ChessServer();
+    	server.start();
+    	ServerManager servManager = new ServerManager(server);
+    	servManager.start();
+    }
+}
